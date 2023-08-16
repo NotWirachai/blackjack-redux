43b3cba9aacfb758ea072a00a15443bbae7e2320
notwirachai <wirachaikh@playwork.co.th>
2023-08-16 11:31:42 +0700

    store devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,16 +12,21 @@
 // export default store;
 
 import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from 'redux-saga'
-import rootReducer from './reducers';
-import rootSaga from './sagas'
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import rootReducer from "./reducers";
+import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(sagaMiddleware)
-);
+const bindMiddleware = (middleware) => {
+  const arrMiddleware = [middleware];
+
+  return composeWithDevTools(applyMiddleware(...arrMiddleware));
+};
+
+const store = createStore(rootReducer, bindMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
